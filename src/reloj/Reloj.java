3797bc507d2a90aa8df2ec3c3ca8ca5134f7e5b7
Bryan Lopez <petitemorth@gmail.com>
2017-08-19 12:12:16 -0500

    Clase Reloj con sus métodos correspondientes y pruebas de como funciona

--- src/reloj/Reloj.java
+++ src/reloj/Reloj.java
@@ -15,6 +15,7 @@ public class Reloj {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        //Si la hora supera los 23 o minutos y segundo superan el 59, se les asignara el valor de 0 por default
         Setup st = new Setup(15, 30, 1);
         System.out.println("Hora Normal: " + st.getHour() + ":" + st.getMinute() + ":" + st.getSecond());
         st.setHour(20);
